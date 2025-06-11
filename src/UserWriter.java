0c7c5654696e2522cc14e86efdc889d829b42152
Egor <zuzik17@yandex.ru>
2025-06-11 19:27:29 +0300

    Added method for calculate and write fianl balance

--- src/UserWriter.java
+++ src/UserWriter.java
@@ -1,24 +1,29 @@
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.nio.file.*;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 public class UserWriter {
     private String outputDir;
+
     public UserWriter(String outputDir) throws IOException {
         this.outputDir = outputDir;
         Files.createDirectories(Paths.get(outputDir)); // Создаём чистую папку
     }
-    public void  writeUsersLog(Map<String,List<Transaction>> mapTransaction){
+
+    public void writeUsersLog(Map<String, List<Transaction>> mapTransaction) {
         for (String key : mapTransaction.keySet()) {
-            writeUserLog(key,mapTransaction.get(key));
+            writeUserLog(key, mapTransaction.get(key));
         }
     }
+
     private void writeUserLog(String username, List<Transaction> logs) {
         Collections.sort(logs); // Сортировка по времени
         String filename = outputDir + "/" + username + ".log";
@@ -27,13 +32,32 @@ public class UserWriter {
             for (Transaction log : logs) {
                 writer.println(log.getString());
             }
-
-            // Добавляем финальный баланс
-//            writer.println(generateFinalBalanceLine(username, calculateFinalBalance(username, logs)));
+            writer.println(writeTotalAmount(username,logs));
+            writer.flush();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private BigDecimal calculateTotalAmount(List<Transaction> logs) {
+        BigDecimal totalAmount = BigDecimal.ZERO;
+        for (Transaction log : logs) {
+            if (log.getOperationType() == OperationType.BALANCE_INQUIRY) {
+                totalAmount = log.getAmount();
+            } else if (log.getOperationType() == OperationType.RECEIVED) {
+                totalAmount = totalAmount.add(log.getAmount());
+            } else {
+                totalAmount = totalAmount.subtract(log.getAmount());
+            }
+        }
+        return totalAmount;
+    }
+
+    private String writeTotalAmount(String user,List<Transaction> logs) {
+        BigDecimal totalAmount = calculateTotalAmount(logs);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+//        [2025-05-10 11:00:03] user001 final balance 1196.92
+        return "["+ LocalDateTime.now().format(formatter) +"] "+user+" final balance "+totalAmount;}
 
 }
 
